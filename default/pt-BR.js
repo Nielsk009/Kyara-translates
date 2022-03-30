4c10941156321e667f40b0b312ec6e80748a9acd
Nielsk009 <102186256+Nielsk009@users.noreply.github.com>
2022-03-30 14:29:35 -0300

    Create pt-BR.js

---
+++ default/pt-BR.js
@@ -0,0 +1,17 @@
+module.exports = {
+   lang: "pt-BR",
+   menção: {
+        titulo: "Oie, Você me chamou",
+        descrição: "Oie, eu sou a ( Kyara ) uma simples bot brasileira, todos os meus comandos são em slash basta utilizar / para vê-los, obtenha mais informações sobre em meu website",
+  },
+   botinfo: {
+         Título: "informações da kyara",
+         descrição: {
+                 Apresentação: "Oie, tudo bom eu sou a ( Kyara ) uma simples bot brasileira com o objetivo de espalhar alegria para todos.",
+                 Informações: "Já fazem [ ] que estou gerenciando [ ] que contém [ ], tenho sono.",
+                 Armazenamento: " Eu estou usando [ ] de minha memória é estou usando [ ] de meu processamento com a latência de [ ]",
+                 Bibliotecas: "Eu fui criada em Javascript utilizando a biblioteca Aoi.js e estou utilizando a database padrão da biblioteca.",
+                 Open-source: "Você sabia que eu sou Open source, para ver meus código acesse meu github e contribua com minha evolução."
+    }
+  },
+}
